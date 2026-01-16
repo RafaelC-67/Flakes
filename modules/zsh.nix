1ef346dfe357cb5f40f9244bb701e519f57d8032
RafaelC-67 <git@zephyremc.xyz>
2026-01-16 18:44:12 +0700

    zsh: Add some configuration
    Add some configuration such as:
    + zoxide
    + auto completion
    + syntax highlightning
    + abbreviation

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -4,11 +4,36 @@
     zsh = {
       enable = true;
 
+      history = {
+        share = true;
+        extended = true;
+        ignoreAllDups = true;
+        ignoreSpace = true;
+      };
+      autocd = true;
+      syntaxHighlighting.enable = true;
+      autosuggestions.enable = true;
+
+      zsh-abbr = {
+        enable = true;
+        abbreviations = {
+          # Nix
+          switch = "nh os switch --ask --impure";
+          clean = "nh clean all";
+          # Misc
+          ff = "fastfetch";
+        };
+      };
+
       shellAliases = {
         cd = "z";
       };
     };
     zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    autin = {
       enable = true;
       enableZshIntegration = true;
     };

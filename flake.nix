ba5540331bf861bb105935a4a5c46bf43a80854b
heavenslab <git@zephyremc.xyz>
2026-01-18 21:51:37 +0700

    flake: Add nvf

--- flake.nix
+++ flake.nix
@@ -24,6 +24,11 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =

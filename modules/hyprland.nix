2376cd20b46bef31d8bd6e923706333e0e5b3ea0
heavenslab <git@zephyremc.xyz>
2026-01-21 16:17:05 +0700

    hypr: Add binds for volume control

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -10,6 +10,12 @@
       bind = SUPER, T, exec, foot
       bind = SUPER, W, exec, firefox
       bind = SUPER, Q, killactive
+
+      bindl = , XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+      bindl = , XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+      bindl = Super+Shift, M, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+      bindle = , XF86AudioRaiseVolume, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ 0; wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
+      bindle = , XF86AudioLowerVolume, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ 0; wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
       # Monitor
       monitor = , preferred, auto, 1
       # Input
